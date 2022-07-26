b6e2a93f58fa10a282b226e05bee45fd09a73615
Lyubov Rodomanova <lrodomanova81@gmail.com>
2022-07-26 20:52:23 +0300

    правки

--- src/test/java/guru/qa/SoapTests.java
+++ src/test/java/guru/qa/SoapTests.java
@@ -11,6 +11,9 @@ import javax.xml.bind.Unmarshaller;
 import java.io.InputStream;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SoapTests {
@@ -37,6 +40,7 @@ public class SoapTests {
         System.out.println(response.asString());
         assertTrue(response.asString().contains("Warsaw"));
         assertTrue(response.asString().contains("PLN"));
+        assertThat(response.asString(), not(containsString("GBP")));
         // TODO implement jaxb unmarshalling
     }
 }
